7ffc0b99ea910de70ed76aa1cf3efab796c56f99
m25112186 <99669367+M25112186@users.noreply.github.com>
2023-05-08 20:58:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# importing modules from flask library
 from flask import Flask , render_template
 
 # creating instance of class Flask, by providing __name__ keyword as argument
@@ -6,42 +5,44 @@ app = Flask(__name__)
 
 # write the routes using decorator functions
 # default route or 'URL'
-@app.route("/my")
+@app.route("/")
 def home():
 
-    name = "Manasvi" # write your name
-    age = "14" # write your age
+    name = "Alex" # write your name
+    age = "12" # write your age
 
     return render_template('index.html' , name = name , age = age)
 
 # define the route to father webpage
 @app.route("/father")
 def father():
-    name="Manish"
-    age="50"
+
+    name = "Vicktor" # write your name
+    age = "40" # write your age
+
     return render_template('father.html' , name = name , age = age)
+
 # define the route to mother webpage
 @app.route("/mother")
 def mother():
-    name="Vandana"
-    age="45"
+
+    name = "Erica" # write your name
+    age = "37" # write your age
+
     return render_template('mother.html' , name = name , age = age)
+
 
 # define the route to friends webpage
 @app.route("/friend")
 def friend():
-    name="Alina"
-    age="15"
+
+    name = "Peter" # write your name
+    age = "12" # write your age
+
     return render_template('friend.html' , name = name , age = age)
 
+
 # add other routes, if you want
-@app.route("/sister")
-def friend():
-    name="Radhika"
-    age="21"
-    return render_template('sister.html' , name = name , age = age)
-
-
 
 
 # run the file
